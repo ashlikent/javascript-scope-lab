1fb7ab6b7fef45b018045294569e1641cfb18e9e
Ashli Briggs <akent23@gmail.com>
2026-04-14 09:29:56 -0500

    scope lab done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+let burgers = [ 'Hamburger' , 'Cheeseburger' ];
+let featuredDrink = 'Strawberryy Milkshake' ;
+function addBurger() {
+    let newBurger = 'Flatburger';
+    burgers.push(newBurger) ;
+}
+if (true) {
+    let anotherNewBurger = 'Maple Bacon Burger' ;
+    burgers.push(anotherNewBurger) ;
+}
+function changeFeatureDrink() {
+    featuredDrink = 'The JavaShake' ;
+}
 // Write your solution in this file!
